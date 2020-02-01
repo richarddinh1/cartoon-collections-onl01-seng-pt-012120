f8b0efea3037974be2f37e8dc2c7dcea2e0c206d
richard dinh <richardndinh@gmail.com>
2020-02-01 22:14:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,15 @@ def summon_captain_planet(planeteer_calls)
 end	
 
 
-def long_planteer_calls(planeteer_calls)
-  planeteer_calls.any? do |call|
-  call.length > 4
-  end
-end	
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
 
 
 def find_the_cheese(snacks)
